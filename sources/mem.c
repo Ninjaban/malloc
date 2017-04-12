2b7e4be95873c97714b2caf62c42016d8671b544
elemarch <lmrchd.e@gmail.com>
2017-04-12 14:19:21 +0200

    [mem.c] ajouté : fonction pour obtenir la taille d'une zone

--- sources/mem.c
+++ sources/mem.c
@@ -12,11 +12,43 @@
 
 #include "malloc.h"
 
-t_mem		ft_mem_init(void)
+/*
+** FONCTION :: ft_mem_get_size
+**  Renvoie la taille de la zone mémoire pointée
+**
+** PARAMETRES
+**  void*	addr		pointeur vers l'adresse dont on veut la taille
+**
+** VALEUR DE RETOUR (size_t)
+**  renvoie 0 si la zone est free ou inexistante, renvoie sa taille sinon.
+*/
+
+size_t		*ft_mem_get_size(void *addr)
+{
+	t_head	*sheet;
+	t_zone	*current;
+
+	if (addr == NULL)
+		return (0);
+	sheet = (t_head)g_mem->addr;
+	while (sheet)
+	{
+		current = sheet->zones;
+		while (current && current->addr != addr)
+			current = current->next;
+		if (current)
+			return (current->size);
+		sheet = sheet->next;
+	}
+	return (0);
+}
+
+//initialise la variable globale
+t_mem		*ft_mem_init(void)
 {
 	t_mem	mem;
 
 	mem.addr = NULL;
 	mem.sz = getpagesize();
-	return (mem);
+	return (&mem);
 }
