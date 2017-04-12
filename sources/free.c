96bca783530898c25fd0b9b7e091e2be5688f73f
Maryse RAJAONARY <mrajaona@e1r3p5.42.fr>
2017-04-12 14:04:03 +0200

    pseudo-code free

--- sources/free.c
+++ sources/free.c
@@ -6,7 +6,7 @@
 /*   By: mrajaona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 11:18:41 by mrajaona          #+#    #+#             */
-/*   Updated: 2017/04/12 12:01:38 by mrajaona         ###   ########.fr       */
+/*   Updated: 2017/04/12 13:59:36 by mrajaona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,54 @@
 */
 
 /*
-** Fonctions appellees :
+** Fonctions appelees :
+** int ft_GETLASIZE(void *addr)
+** 	renvoie la taille de memoire allouee a l'adresse "ptr"
+**
 ** int munmap(void *addr, size_t len)
 ** 	<sys/mman.h>
 ** 	success : 0 ; falure : -1, set errno
 ** 	"addr" must be a multiple of the page size
-** 	"len" is the length of the mapping
+** 	"len" is the length of the mapping in bytes ?
 */
+
+int		ft_empty_page(void *addr) // debut ? header ?
+{
+	size_t	n;
+
+	n = 0;
+	while (DANS_LA_PAGE) // n < sz ?
+	{
+		if (val != 0)
+			return (FALSE);
+		n++;
+	}
+	return (TRUE);
+}
 
 void	free(void *ptr)
 {
+	size_t	len; // n'inclut pas le header
+	size_t	n;
+	void	*page;
+	int		sz; // = getpagesize() ?
+
 	if (!ptr)
 		return ;
-	/* set all to 0 */
-	/* unmap */
+	len = ft_GETLASIZE(ptr);
+	link_prev_next_zone();
+	set_ptr_header_to_0();
+	n = 0;
+	while (n < len)
+	{
+		ptr[n] = 0;
+		n++;
+	}
+	if (ft_empty_page(page))
+	{
+		link_prev_next_page(page);
+		set_page_header_to_0(page);
+		set_page_end_to_0(page);
+		munmap(page, sz);
+	}
 }
