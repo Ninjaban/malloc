3f3df2bd503d7ccf667bf3e3030a2dcc35c65a2c
Maryse RAJAONARY <mrajaona@e1r3p3.42.fr>
2017-04-25 13:30:59 +0200

    je suis pas sure que ca marche...

--- sources/realloc_tools.c
+++ sources/realloc_tools.c
@@ -6,7 +6,7 @@
 /*   By: mrajaona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/25 11:34:15 by mrajaona          #+#    #+#             */
-/*   Updated: 2017/04/25 12:12:33 by mrajaona         ###   ########.fr       */
+/*   Updated: 2017/04/25 13:26:54 by mrajaona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 ** Cherche la zone correspondant à l'adresse
 ** Retourne NULL si aucune zone ne correspond
 */
-t_zone	*ft_fetch_zone(void *addr)
+t_zone		*ft_fetch_zone(void *addr)
 {
 	t_head	*head;
 	t_zone	*zone;
@@ -37,10 +37,10 @@ t_zone	*ft_fetch_zone(void *addr)
 ** Cherche le header correspondant à la zone
 ** Retourne NULL si aucun header ne correspond
 */
-t_head	*ft_fetch_head(t_zone *zone)
+static t_head	*ft_fetch_head(t_zone *zone)
 {
-	t_head	*head;
-	t_zone	*z;
+	t_head		*head;
+	t_zone		*z;
 
 	if (!zone || !g_mem)
 		return (NULL);
@@ -57,19 +57,15 @@ t_head	*ft_fetch_head(t_zone *zone)
 /*
 ** Retourne TRUE si il reste assez de place dans la zone réservée, FALSE sinon
 */
-char	ft_zone_fit(size_t size, t_zone *zone)
+char			ft_zone_fit(size_t size, t_zone *zone)
 {
-	size_t	sz;
-	t_head	*head;
+	t_head		*head;
 
 	if (size <= zone->size)
 		return (TRUE);
 	if (zone->next)
-		sz = (size_t)(zone->next->addr - zone->addr);
-	else
-	{
-		head = ft_fetch_head(zone);
-		size = head->size - (size_t)(head->zones - zone);
-	}
-	return (FALSE);
+		return (size <= (size_t)(zone->next->addr - zone->addr) ? TRUE : FALSE);
+	head = ft_fetch_head(zone);
+	return (size <= (head->size - (size_t)(zone->addr - head->zones->addr)) ?
+			TRUE : FALSE);
 }
