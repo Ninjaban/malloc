eff89af4de015d03ebc7783c4b397838c8db9902
Jonathan Carra <jcarra@student.42.fr>
2018-01-11 16:06:14 +0100

    ajout de tests

---
+++ tests/test0.c
@@ -0,0 +1,18 @@
+//
+// Created by Jonathan CARRA on 1/10/18.
+//
+
+#include "malloc.h"
+
+int main()
+{
+    int i;
+    char *addr;
+
+    i = 0;
+    while (i < 1024)
+    {
+        i++;
+    }
+    return (0);
+}
---
+++ tests/test1.c
@@ -0,0 +1,20 @@
+//
+// Created by Jonathan CARRA on 1/10/18.
+//
+
+#include "malloc.h"
+
+int main()
+{
+	int i;
+	char *addr;
+
+	i = 0;
+	while (i < 1024)
+	{
+		addr = (char*)malloc(1024);
+		addr[0] = 42;
+		i++;
+	}
+	return (0);
+}
